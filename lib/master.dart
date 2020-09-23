d96a83f42a6e7c42beaa7420b43cf16eaffa958f
FrankKamala <frankkamala@gmail.com>
2020-09-23 12:40:35 +0300

    add Orientation Builder on master

---
+++ lib/master.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Master extends StatefulWidget{
+  @override
+  _MasterState createState() => _MasterState();
+}
+
+class _MasterState extends State<Master>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+   return Scaffold(
+    appBar: AppBar(
+
+    ),
+     body: OrientationBuilder(builder: (context, orientation) {
+
+
+     }),
+
+   );
+  }
+}
