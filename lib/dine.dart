7a76d27447ae5fccc8eec15d5fae3f63f82e463d
FrankKamala <frankkamala@gmail.com>
2020-09-29 16:11:49 +0300

    add front and dinning screen

---
+++ lib/dine.dart
@@ -0,0 +1,15 @@
+
+    import 'package:flutter/material.dart';  
+      
+    class DiningRoomScreen extends StatelessWidget {  
+      @override  
+      Widget build(BuildContext context) {  
+        return Container(  
+          child: Center(  
+            child: Text('DINING ROOM',  
+                style: TextStyle(fontSize: 32.0),  
+              )  
+          ),  
+        );  
+      }  
+    }
---
+++ lib/front.dart
@@ -0,0 +1,14 @@
+  import 'package:flutter/material.dart';  
+      
+    class FrontRoomScreen extends StatelessWidget {  
+      @override  
+      Widget build(BuildContext context) {  
+        return Container(  
+          child: Center(  
+            child: Text('FRONT ROOM',  
+            style: TextStyle(fontSize: 35.0),  
+            ),  
+          ),  
+        );  
+      }  
+    }
